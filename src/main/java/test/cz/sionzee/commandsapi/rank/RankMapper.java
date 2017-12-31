1ddca960f5b36481821089c0f2f42631fce81b43
Jan "siOnzee" Tržický <jan@sionzee.cz>
2018-01-01 00:47:40 +0100

    A little fixes

--- src/main/java/test/cz/sionzee/commandsapi/rank/RankMapper.java
+++ src/main/java/test/cz/sionzee/commandsapi/rank/RankMapper.java
@@ -4,25 +4,25 @@ import cz.sionzee.commandsapi.mappers.IArgumentConverter;
 import cz.sionzee.commandsapi.mappers.IArgumentMapper;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
 import java.util.IdentityHashMap;
 import java.util.Map;
 
 public class RankMapper implements IArgumentMapper {
-    private IdentityHashMap<Class, IArgumentConverter> m_map;
+    private IdentityHashMap<Class<?>, IArgumentConverter<?>> m_map;
 
     @NotNull
     @Override
     public Map<Class<?>, IArgumentConverter<?>> getMapper() {
-        return Collections.unmodifiableMap(m_map);
+        return m_map;
     }
 
     @Override
     public void init() {
-        m_map = new IdentityHashMap<Class, IArgumentConverter>();
+        m_map = new IdentityHashMap<>(1);
         m_map.put(Rank.class, Rank::from);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public <V> V convertTo(@NotNull Class<V> key, @NotNull String arg) {
         return (V) m_map.get(key).from(arg);
